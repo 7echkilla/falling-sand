909e7d12760aa40cb5f2d786bd6b087200b67544
7echkilla <prithik1510@gmail.com>
2026-04-12 00:33:22 +1000

    Major updates: - Adopt production-level project structure - Modular code for scalability and maintainability - Add database to manage users and contribution data - Move html-type returns to templates

---
+++ app/github.py
@@ -0,0 +1,84 @@
+import requests
+
+def get_token(authorisation_code, client_id, client_secret):
+    """
+    Convert temporary login code to usable API token 
+    """
+
+    url = "https://github.com/login/oauth/access_token"
+    headers = {"Accept": "application/json"}
+    payload = {
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "code": authorisation_code
+    }
+
+    response = requests.post(url=url, json=payload, headers=headers).json()
+
+    if ("access_token" not in response):
+        raise Exception(f"OAuth failed: {response}")
+    access_token = response["access_token"]
+
+    return access_token
+
+def get_username(token):
+    """
+    Fetch username using authorisation token
+    """
+
+    url = "https://api.github.com/graphql"
+    headers = {"Authorization": f"Bearer {token}"}
+    query = """ { 
+        viewer { 
+            login
+        } 
+    } """
+
+    response = requests.post(url=url, json={"query": query}, headers=headers).json()
+    username = response["data"]["viewer"]["login"]
+    
+    return username 
+
+def fetch_contributions(username, token):
+    """
+    Request user contributions via GitHub's GraphQL API
+    """
+
+    url = "https://api.github.com/graphql"
+    headers = {"Authorization": f"Bearer {token}"}
+    query = f"""
+        query {{
+            user(login: "{username}") {{
+                contributionsCollection {{
+                contributionCalendar {{
+                    weeks {{
+                    contributionDays {{
+                        date
+                        contributionCount
+                    }}
+                    }}
+                }}
+                }}
+            }}
+        }}
+    """
+
+    response = requests.post(url=url, json={"query": query}, headers=headers)
+    if (response.status_code != 200):
+        raise Exception("GitHub API request failed")
+    
+    response = response.json()
+
+    try:
+        weeks = response["data"]["user"]["contributionsCollection"]["contributionCalendar"]["weeks"]
+    except KeyError:
+        raise Exception("Invalid GitHub username or API response")
+
+    # Flatten data as list comprehension (faster than nested for loops)
+    days = [
+        {"date": day["date"], "count": day["contributionCount"]}
+        for week in weeks
+        for day in week["contributionDays"]
+    ]
+
+    return days
---
+++ app/routes.py
@@ -0,0 +1,61 @@
+from flask import Blueprint, request, send_file, jsonify, redirect
+from app import config
+from app.github import fetch_contributions, get_username, get_token, render_template
+from app.gif import generate_gif_bytes
+from app.db import get_token, save_user
+
+main = Blueprint("main", __name__)  # Organise routes into a module
+
+@main.route("/")
+def home():
+    return render_template("home.html")
+
+@main.route("/login")
+def login():
+    redirect_uri = request.url_root + "callback"    # https://falling-sand.onrender.com/callback
+
+    url = (
+        "https://github.com/login/oauth/authorize"
+        f"?client_id={config.CLIENT_ID}"            # Indentifies app to GitHub
+        "&scope=read:user"                          # Permission to read user information
+        f"&redirect_uri={redirect_uri}"             # Post login redirect
+    )
+
+    return redirect(url)
+
+@main.route("/callback")
+def callback():
+    authorisation_code = request.args.get("code")   # Temporary code sent by GitHub as proof
+
+    # Exchange code for access token
+    access_token = get_token(
+        authorisation_code,
+        config.CLIENT_ID,
+        config.CLIENT_SECRET
+    )
+
+    # Store user to avoid recurring login
+    username = get_username(access_token)
+    save_user(username, access_token)
+
+    return render_template("success.html", username=username, url_root=request.url_root)
+
+@main.route("/gif")
+def gif():
+    # Fetch parameters from: /gif?username=abc&key=123
+    username = request.args.get("username")
+    key = request.args.get("key")
+
+    # Prevent abuse of API by unauthorised users
+    if (key != config.GIF_SECRET):
+        return jsonify({"error": "Unauthorized"}), 403
+
+    # Reject non-logged in users
+    access_token = get_token(username)
+    if (not access_token):
+        return jsonify({"error": "User not logged in"}), 401
+
+    days = fetch_contributions(username, access_token)
+    gif_bytes = generate_gif_bytes(days)
+
+    return send_file(gif_bytes, mimetype="image/gif")
